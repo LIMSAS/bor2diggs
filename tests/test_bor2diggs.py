18e39923b712da97745e43ed998f58b9fd9462f5
Salem Harrache <salem@harrache.info>
2025-02-05 11:53:37 +0100

    WIP

--- tests/test_bor2diggs.py
+++ tests/test_bor2diggs.py
@@ -1,5 +1,6 @@
 from pytest_cases import pytest_fixture_plus
 
+import pydiggs
 import bor2diggs
 
 from . import INPUT_BOR_FILES
@@ -20,4 +21,19 @@ def bor_filename(request):
 def test_diggs_export(bor_filename, tmp_path):
     output_filename = tmp_path / "output.diggs.xml"
     output_filename.write_text(bor2diggs.convert_to_diggs(bor_filename))
+    assert_same_files(output_filename, bor_filename.with_suffix(".diggs.xml"))
+
+
+def test_diggs_validation(bor_filename, tmp_path):
+    output_filename = tmp_path / "output.diggs.xml"
+    output_filename.write_text(bor2diggs.convert_to_diggs(bor_filename))
+    validation = pydiggs.validator(output_filename)
+
+    __import__("pdb").set_trace()
+    # Schema validation (using default DIGGS Schema v2.6)
+    validation.schema_check()
+
+    # Dictionary validation (using default DIGGS Dictionary)
+    validation.dictionary_check()
+
     assert_same_files(output_filename, bor_filename.with_suffix(".diggs.xml"))
