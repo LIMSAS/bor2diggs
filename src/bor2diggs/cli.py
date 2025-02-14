6aba64bb96d69e36705b22d28beae93a69c30b6f
Salem Harrache <salem@harrache.info>
2025-02-14 10:22:59 +0100

    Fix file output option

--- src/bor2diggs/cli.py
+++ src/bor2diggs/cli.py
@@ -5,9 +5,7 @@ from .convert import convert_to_diggs
 
 @click.command()
 @click.argument("bor_input", type=click.File("rb"))
-@click.option(
-    "-o", "--output", type=click.Path(writable=True, dir_okay=False), required=False
-)
+@click.option("-o", "--output", type=click.Path(writable=True, dir_okay=False), required=False)
 def main(bor_input, output):
     """Convert BOR file to a DIGGS."""
     diggs_content = convert_to_diggs(bor_input)
@@ -15,7 +13,8 @@ def main(bor_input, output):
         # sys.stdout.buffer
         print(diggs_content)
     else:
-        output.write(diggs_content)
+        with click.open_file(output, "w") as f:
+            f.write(diggs_content)
 
 
 if __name__ == "__main__":
